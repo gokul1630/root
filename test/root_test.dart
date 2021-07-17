b7b79c610a28255a035ece972df2ed0694a419ca
gokul1630 <gokulgp163034@gmail.com>
2021-07-17 14:15:59 +0530

    remove unused import

--- test/root_test.dart
+++ test/root_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:root/root.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('root');
